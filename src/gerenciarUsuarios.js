ed0d193ae7833fdcf0203dd4291be1d272a5dcff
Alsestrela <alsestrela@gmail.com>
2025-09-09 22:56:49 -0300

    Mudei a lista de nomes de usuarios para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,5 +1,36 @@
+/* 
+Modelo de um objeto
+[
+ {
+    primeiraPropriedade: 'Valor'
+    segundaPropriedade: 'Valor'
+ },
+ {
+    primeiraPropriedade: 'Valor'
+    segundaPropriedade: 'Valor'
+ }
+]
+*/
+
 // constante global
-const usuarios = ['Matheus', 'Lucas', 'Marcos', 'João'];
+const usuarios = [
+  {
+    nome: 'Matheus',
+    email:'matheus@email.com'
+  },
+  {
+    nome: 'Lucas',
+    email:'lucas@email.com'
+  },
+  {
+    nome: 'Marcos',
+    email:'marcos@email.com'
+  },
+  {
+    nome: 'João',
+    Email:'joao@email.com'
+  }
+];
 
 //Retorna uma lista de nomes gravados na lista de nomes de usuários
 function listarUsuarios() {
@@ -8,8 +39,8 @@ function listarUsuarios() {
 
 //Acrescenta um novo nome de usuário passado no momento da chamada da função ao fim da lista
 // comando push - adicionar nome no final da fila
-function adicionarNovoUsuario (nome) {
-    usuarios.push(nome);
+function adicionarNovoUsuario (usuario) {
+    usuarios.push(usuario);
 } 
 
 //compartilhar funções para teste
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -1,20 +1,32 @@
-//Importar as funções que serão testadas
+//Importa as funções que serão testadas
 const { listarUsuarios } = require('../src/gerenciarUsuarios');
 const { adicionarNovoUsuario } = require('../src/gerenciarUsuarios');
 
-//Importar comando que será usado da biblioteca chai
+//Importa comando que será usado da biblioteca chai
 const { expect } = require('chai');
 
 describe ('Testes das funções de Gestão de Usuários', function() {
     it ('Validar que posso adicionar um novo nome lista de usuarios ', function () {
 
-        //1.Adicionar um novo nome na lista de usuários
-        adicionarNovoUsuario ('Luzia');
+        //1.Adiciona um novo nome na lista de usuários
+        adicionarNovoUsuario ({
+            nome:'Luzia',
+            email:'luzia@email.com'
+        });
         
         //2.Retornar a lista de usuários na caixa lista de usuarios
         const listaDeUsuarios = listarUsuarios();
 
-        //3.comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Luzia');
+        //3.compara se o novo nome/objeto está no fim da lista de usuários
+        //to.equal = compara 2 valores // at(-1) = está na posição x na lista // at(-0) = primeira posição
+
+        //expect(listaDeUsuarios.at(-1).nome).to.equal('Luzia'); 
+        //expect(listaDeUsuarios.at(-1).email).to.equal('luzia@email.com');
+        
+        //to.deep.equal = to.eql usado para comparar objeto
+        expect(listaDeUsuarios.at(-1)).to.eql({  
+            nome:'Luzia',
+            email:'luzia@email.com'
+        });
     });
  });
